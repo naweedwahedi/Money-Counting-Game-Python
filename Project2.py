885727b3f9a052ef5a3a03e75e4c7a60d7b5534b
naweed <naweedwahedi2011@gmail.com>
2020-01-26 22:33:12 -0800

    First Commit

--- Project2.py
+++ Project2.py
@@ -1,9 +1,3 @@
-#Money Counting Game
-#Create a change-counting game that gets the user to enter the number of coins required 
-#to make exactly one dollar. 
-#The program should prompt the user to enter the number of pennies, nickels, dimes,and quarters. 
-#If the total value of the coins entered is equal to one dollar, the program should congratulate the user for winning the game.
-#Otherwise, the program should display a message indicating whether the amount entered was more than or less than one dollar.
 
 penny_Value = 1
 nickel_Value = 5
